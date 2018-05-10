083695026ad8fd03ff9a73c2fdf0fde4fd5f4a8a
Cyril GIBAUD <cyril.gibaud@toonkit-studio.com>
2018-05-10 18:29:17 +0200

    Initial commit

---
+++ DISTRIB.bat
@@ -0,0 +1,14 @@
+@echo off
+
+if [%1]==[] (
+	set /p TMP="DISTRIB.bat Must be called with a valid ouput path as first argument !"
+	exit /b
+)
+
+if [%2]==[] (
+	set /p TMP="DISTRIB.bat Must be called with a Maya Name as second argument!"
+	exit /b
+)
+
+robocopy /S /NFL /NDL /NJH *.* %~dp0\Maya %1\%2 /XF DISTRIB.bat /XD examples
+robocopy /MIR /S /NFL /NDL /NJH *.* %~dp0\Preferences %1\Preferences
